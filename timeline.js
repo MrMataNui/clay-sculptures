1b81faca2c12190fc64a1f72366e7de63f344cf8
Tyler Tolson <tylertolson1@gmail.com>
2019-09-30 12:19:54 -0400

    turned the links into specific months; up-to-date

--- timeline.js
+++ timeline.js
@@ -1,16 +1,33 @@
-	$(() => {
-	  // $().timeline({ orientation: 'vertical', issuesSpeed: 300, datesSpeed: 100, arrowKeys: 'true', startAt: 3 });
+$(() => {
+	// $().timeline({ orientation: 'vertical', issuesSpeed: 300, datesSpeed: 100, arrowKeys: 'true', startAt: 3 });
 
-	  function onClick(date) {
-	    $('#dates a').removeClass('selected');
-	    $('#issues li').removeClass('selected');
+	$('a[href="#2019jan"]').click(() => onClick('2019jan'));
+	$('a[href="#2019mar"]').click(() => onClick('2019mar'));
+	$('a[href="#2019may"]').click(() => onClick('2019may'));
+	$('a[href="#2019aug"]').click(() => onClick('2019aug'));
+	$('a[href="#2019sep"]').click(() => onClick('2019sep'));
 
-	    $(`a[href="#${date}"]`).addClass('selected');
-	    $(`#issues li#${date}`).addClass('selected');
-	  }
-	  $('a[href="#2019jan28"]').click(() => onClick('2019jan28'));
-	  $('a[href="#2019mar15"]').click(() => onClick('2019mar15'));
-	  $('a[href="#2019may02"]').click(() => onClick('2019may02'));
-	  $('a[href="#2019may29"]').click(() => onClick('2019may29'));
-	  $('a[href="#2019aug05"]').click(() => onClick('2019aug05'));
-	});
+	$('#previous-year').click(() => yearChange('previous-year'));
+	$('#following-year').click(() => yearChange('following-year'));
+});
+
+function onClick(date) {
+	$('#dates a').removeClass('selected');
+	$('#issues li').removeClass('selected');
+
+	$(`a[href="#${date}"]`).addClass('selected');
+	$(`#issues li#${date}`).addClass('selected');
+	// $('#issues li').addClass('deselected');
+	// $(`#issues li#${date}`).removeClass('deselected');
+}
+
+function yearChange(date) {
+	const getYears = [2019];
+	let number = $('#date2019 n').html();
+	number = parseInt($.trim(number));
+	switch (date) {
+		case 'previous-year':		if (getYears.includes(number - 1)) { number--; }		break;
+		case 'following-year':	if (getYears.includes(number + 1)) { number++; }	break;
+	}
+	$(`#date2019 n`).html(number);
+}
