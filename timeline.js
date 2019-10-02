555790d5764f7ca55e0831d80c42851433a49408
Tyler Tolson <tylertolson1@gmail.com>
2019-10-02 14:18:44 -0400

    Added an 'Inktober' section

--- timeline.js
+++ timeline.js
@@ -1,33 +1,91 @@
 $(() => {
 	// $().timeline({ orientation: 'vertical', issuesSpeed: 300, datesSpeed: 100, arrowKeys: 'true', startAt: 3 });
 
-	$('a[href="#2019jan"]').click(() => onClick('2019jan'));
-	$('a[href="#2019mar"]').click(() => onClick('2019mar'));
-	$('a[href="#2019may"]').click(() => onClick('2019may'));
-	$('a[href="#2019aug"]').click(() => onClick('2019aug'));
-	$('a[href="#2019sep"]').click(() => onClick('2019sep'));
+	// initialClass();
 
-	$('#previous-year').click(() => yearChange('previous-year'));
-	$('#following-year').click(() => yearChange('following-year'));
+	$('[id$=Inktober]').css('display', 'none');
+	const inktoberList = [1];
+	for (let day in inktoberList) {
+		day = inktoberList[day];
+		$(`#day-${day}-Inktober`).css('display', 'block');
+	}
+
+	$('[class^=Inktober-day-de]').after('<br />');
+	$('#lang-en').click(() => langChange('en'));
+	$('#lang-de').click(() => langChange('de'));
+
+	$('#2019jan').click(() => onClick('2019jan'));
+	$('#2019mar').click(() => onClick('2019mar'));
+	$('#2019may').click(() => onClick('2019may'));
+	$('#2019aug').click(() => onClick('2019aug'));
+	$('#2019sep').click(() => onClick('2019sep'));
+	$('#2019oct').click(() => onClick('2019oct'));
+
+	$('[class^=Inktober-day]').css('display', 'none');
+	$('[id^=Inktober-day]').click(function() {
+		inktober($(this));
+	});
+
+	// $('#previous-year').click(() => yearChange('previous-year'));
+	// $('#following-year').click(() => yearChange('following-year'));
 });
 
-function onClick(date) {
-	$('#dates a').removeClass('selected');
-	$('#issues li').removeClass('selected');
+function inktober(location) {
+	const id = location.attr('id');
+	const classDisplay = $(`[class^=${id}]`).css('display');
+	const display = classDisplay === 'none' ? 'block' : 'none';
+	$('[class^=Inktober-day]').css('display', 'none');
+	$(`div.${id}`).css('display', display);
+}
 
-	$(`a[href="#${date}"]`).addClass('selected');
-	$(`#issues li#${date}`).addClass('selected');
-	// $('#issues li').addClass('deselected');
-	// $(`#issues li#${date}`).removeClass('deselected');
+function initialClass() {
+	$('#months li').addClass('deselected');
+	$('#2019jan').addClass('selected');
+	$('#months li#2019jan').addClass('selected');
+	$('#months li#2019jan').removeClass('deselected');
+}
+
+function langChange(lang) {
+	$('.en').css('display', 'none');
+	$('.de').css('display', 'none');
+	switch (lang) {
+		case 'en':
+			$('.en').css('display', 'block');
+			$('#lang-name').html('Language: English');
+			break;
+		case 'de':
+			$('.de').css('display', 'block');
+			$('#lang-name').html('Sprache: Deutsch');
+			break;
+	}
+}
+
+function onClick(date) {
+	$('#dates div').removeClass('selected');
+	$('#months li').removeClass('selected');
+
+	$(`#${date}`).addClass('selected');
+	$(`#months li#${date}`).addClass('selected');
+
+	// $('#months li').addClass('deselected');
+	// $(`#months li#${date}`).removeClass('deselected');
 }
 
 function yearChange(date) {
-	const getYears = [2019];
+	const getYears = [2019, 2020];
 	let number = $('#date2019 num').html();
 	number = parseInt($.trim(number));
 	switch (date) {
-		case 'previous-year':		if (getYears.includes(number - 1)) { number--; }		break;
-		case 'following-year':	if (getYears.includes(number + 1)) { number++; }	break;
+		case 'previous-year':
+			if (getYears.includes(number - 1)) {
+				number--;
+			}
+			break;
+		case 'following-year':
+			if (getYears.includes(number + 1)) {
+				number++;
+			}
+			break;
 	}
 	$('#date2019 num').html(number);
 }
